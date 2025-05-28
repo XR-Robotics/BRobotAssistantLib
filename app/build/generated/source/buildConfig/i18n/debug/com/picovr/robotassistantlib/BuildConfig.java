fa999464fd3137ba9c9240e2e931b2c44aab49ba
BigTrue221 <pengren@bytedance.com>
2025-05-28 21:19:32 +0800

    Open source requirements for robot assistants v1.0
    Open source requirements for robot assistants.

---
+++ app/build/generated/source/buildConfig/i18n/debug/com/picovr/robotassistantlib/BuildConfig.java
@@ -0,0 +1,11 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.picovr.robotassistantlib;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.picovr.robotassistantlib";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "i18n";
+}
